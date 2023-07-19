948c1db53bfb434d9e760326f8f3ba2ceaf988da
tgrangeo <thomas.grangeon9@gmail.com>
2023-07-19 18:55:03 +0200

    slack-go look better ???

--- main.go
+++ main.go
@@ -2,38 +2,109 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
-	"github.com/shomali11/slacker"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
-func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
-	for evenmt := range analyticsChannel {
-		fmt.Println("Command events")
-		fmt.Println(evenmt.Timestamp)
-		fmt.Println(evenmt.Command)
-		fmt.Println(evenmt.Parameters)
-		fmt.Println(evenmt.Event)
-		fmt.Println()
+func HandleAppMentionEventToBot(event *slackevents.AppMentionEvent, client *slack.Client) error {
+
+	user, err := client.GetUserInfo(event.User)
+	if err != nil {
+		return err
 	}
+
+	text := strings.ToLower(event.Text)
+
+	attachment := slack.Attachment{}
+
+	if strings.Contains(text, "hello") || strings.Contains(text, "hi") {
+		attachment.Text = fmt.Sprintf("Hello %s", user.Name)
+		attachment.Color = "#4af030"
+	} else if strings.Contains(text, "weather") {
+		attachment.Text = fmt.Sprintf("Weather is sunny today. %s", user.Name)
+		attachment.Color = "#4af030"
+	} else {
+		attachment.Text = fmt.Sprintf("I am good. How are you %s?", user.Name)
+		attachment.Color = "#4af030"
+	}
+	_, _, err = client.PostMessage(event.Channel, slack.MsgOptionAttachments(attachment))
+	if err != nil {
+		return fmt.Errorf("failed to post message: %w", err)
+	}
+	return nil
+}
+
+func HandleEventMessage(event slackevents.EventsAPIEvent, client *slack.Client) error {
+	switch event.Type {
+	case slackevents.CallbackEvent:
+		innerEvent := event.InnerEvent
+		switch evnt := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			err := HandleAppMentionEventToBot(evnt, client)
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return errors.New("unsupported event type")
+	}
+	return nil
 }
 
 func main() {
+
 	godotenv.Load(".env")
-	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
-	go printCommandEvents(bot.CommandEvents())
-	bot.Command("ping", &slacker.CommandDefinition{
-		Handler: func(bc slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
-			w.Reply("pong")
-		},
-	})
+
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	appToken := os.Getenv("SLACK_APP_TOKEN")
+
+	client := slack.New(token, slack.OptionDebug(true), slack.OptionAppLevelToken(appToken))
+
+	socketClient := socketmode.New(
+		client,
+		socketmode.OptionDebug(true),
+		socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+
 	ctx, cancel := context.WithCancel(context.Background())
+
 	defer cancel()
-	err := bot.Listen(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	go func(ctx context.Context, client *slack.Client, socketClient *socketmode.Client) {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("Shutting down socketmode listener")
+				return
+			case event := <-socketClient.Events:
+
+				switch event.Type {
+
+				case socketmode.EventTypeEventsAPI:
+
+					eventsAPI, ok := event.Data.(slackevents.EventsAPIEvent)
+					if !ok {
+						log.Printf("Could not type cast the event to the EventsAPI: %v\n", event)
+						continue
+					}
+
+					socketClient.Ack(*event.Request)
+					err := HandleEventMessage(eventsAPI, client)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+		}
+	}(ctx, client, socketClient)
+
+	socketClient.Run()
 }
